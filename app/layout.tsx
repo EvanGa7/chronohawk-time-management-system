910248f631d7db649ace01ffddbd3ac9e9c4e936
EvanGardner721 <evan.gardner75@gmail.com>
2023-09-26 00:15:50 -0400

    I connected to the database using supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          {children}
+          <Footer />
+        </Providers>
+      </body>
     </html>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from 'tailwindcss'
 
+const {nextui} = require("@nextui-org/react");
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -14,7 +17,37 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      'ribbon': {
+        50: '#FEF0F1',
+        100: '#FFE2E3',
+        200: '#FFCACE',
+        300: '#FE9EA8',
+        400: '#FF687B',
+        500: '#FF3352',
+        600: '#F01944',
+        700: '#A90832',
+        800: '#900B31',
+        900: '#510015',
+        950: '#510015',
+      },
+      'buddha': {
+          50: '#FCFBEA',
+          100: '#FAF7C7',
+          200: '#F6EC92',
+          400: '#EAC725',
+          500: '#E6B919',
+          600: '#BC8912',
+          700: '#966312',
+          800: '#7D4F16',
+          900: '#6A4119',
+          950: '#3E220A',
+        },
+    }
   },
-  plugins: [],
+  
+  darkMode: "class",
+
+  plugins: [nextui()],
 }
 export default config
