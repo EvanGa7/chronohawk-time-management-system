d1a106baf571e8c2299db75f4438813b1a67c697
EvanGardner721 <evan.gardner912@gmail.com>
2023-10-15 15:24:51 -0400

    updates to the calendar page

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -9,13 +9,12 @@ import { useRouter } from 'next/navigation'; // Import useRouter hook
 import { createClient } from '@supabase/supabase-js';
 import InModal from '../components/inModal';
 import TaskModal from '../components/taskModal';
+import '../globals.css';
 
 const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
-const MAX_EVENT_HEIGHT = 80;
 
 interface Task {
   title: string;
@@ -25,6 +24,10 @@ interface Task {
   priorityof: number;
   estimatedtime: number;
   taskid: string;
+  tasktype: number;
+  numdays: number;
+  eventHeight: number;
+  color: string;
 }
 
 export default function Calendar() {
@@ -79,76 +82,263 @@ export default function Calendar() {
     }
   });
 
-  
+//   function calculateTaskDuration(taskDuration, startDate, dueDate, numdays) {
+//     let remainingDuration = taskDuration;
+//     let currentDate = new Date(startDate);
+//     let endDate = new Date(startDate);
 
-  function calculateTaskDuration(taskDuration, startDate, dueDate) {
-    let remainingDuration = taskDuration;
-    let currentDate = new Date(startDate);
-    let endDate = new Date(startDate);
+//     let daysScheduled = 0;
 
-    while (remainingDuration > 0) {
-        const dayOfWeek = currentDate.getDay();
-        const availableTime = freeTimeByDay[dayOfWeek];
+//     while (daysScheduled < numdays) {
+//         const dayOfWeek = currentDate.getDay();
+//         const availableTime = freeTimeByDay[dayOfWeek] || 0;
 
-        // Check if the current date exceeds the due date
-        if (dueDate && currentDate > dueDate) {
-            console.error("Task cannot be scheduled within the due date!");
-            return null; // or handle this case as you see fit
-        }
+//         if (dueDate && currentDate > dueDate) {
+//             console.error("Task cannot be scheduled within the due date!");
+//             return null;
+//         }
 
-        if (availableTime >= remainingDuration) {
-          endDate.setMinutes(endDate.getMinutes() + remainingDuration);
-          remainingDuration = 0;
-        } else {
-            endDate.setMinutes(23, 59); // Set to end of day
-            remainingDuration -= availableTime;
-            currentDate.setDate(currentDate.getDate() + 1);
-            endDate = new Date(currentDate);
-        }      
-    }
+//         if (remainingDuration > 0) {
+//             if (availableTime >= remainingDuration) {
+//                 endDate.setMinutes(endDate.getMinutes() + remainingDuration);
+//                 remainingDuration = 0;
+//             } else {
+//                 endDate.setMinutes(endDate.getMinutes() + availableTime);
+//                 remainingDuration -= availableTime;
+//             }
+//         }
 
-    return {
-        start: startDate,
-        end: endDate,
-    };
+//         if (remainingDuration <= 0 && daysScheduled === numdays - 1) {
+//             break;
+//         }
+
+//         currentDate.setDate(currentDate.getDate() + 1);
+//         endDate = new Date(currentDate);
+//         daysScheduled++;
+//     }
+
+//     endDate.setDate(endDate.getDate() - 1);
+
+//     return {
+//         start: startDate,
+//         end: endDate,
+//     };
+// }
+
+// function deductTimeFromTracker(freeTimeTracker, day, time) {
+//   const dayInfo = freeTimeTracker.find(d => d.day === day);
+//   if (dayInfo) {
+//       dayInfo.availableTime -= time;
+//   }
+// }
+
+// function isTimeAvailable(freeTimeTracker, day, time) {
+//   const dayInfo = freeTimeTracker.find(d => d.day === day);
+//   return dayInfo && dayInfo.availableTime >= time;
+// }
+
+
+// function initializeFreeTimeTracker(freeTimeByDay) {
+//   return Object.keys(freeTimeByDay).map(day => ({
+//       day: parseInt(day),
+//       availableTime: freeTimeByDay[day]
+//   }));
+// }
+
+// async function updateTasksWithDates() {
+//   const sortedTasks = [...tasks].sort((a, b) => {
+//       if (a.duedate < b.duedate) return -1;
+//       if (a.duedate > b.duedate) return 1;
+//       return b.priorityof - a.priorityof;
+//   });
+
+//   let currentDate = new Date();
+//   const freeTimeTracker = initializeFreeTimeTracker(freeTimeByDay);
+
+//   let recalculationCount = 0;
+//   const MAX_RECALCULATIONS = 3;
+
+//   for (let i = 0; i < sortedTasks.length; i++) {
+//       let task = sortedTasks[i];
+
+//       const { start: taskStartDate, end: taskEndDate } = calculateTaskDuration(task.estimatedtime, currentDate, task.duedate, task.numdays);
+
+//       // If endDate is before startDate, reset and recalculate
+
+//         if (taskEndDate < taskStartDate) {
+//           if (recalculationCount >= MAX_RECALCULATIONS) {
+//               console.error("Max recalculations reached for task", task.name);
+//                         // Reset currentDate to the next day after the previously scheduled task's endDate
+//           if (i > 0) {
+//               currentDate = new Date(sortedTasks[i - 1].end);
+//               currentDate.setDate(currentDate.getDate() + 1);
+//               continue
+//           }
+//           i--; // Decrement index to recalculate the current task
+//           }
+//           recalculationCount++;
+//         } else {
+//             recalculationCount = 0;  // reset count for the next task
+//         }
+//       }
+
+//       // Deduct the time used by the task from the freeTimeTracker
+//       let timeUsed = (taskEndDate.getHours() * 60 + taskEndDate.getMinutes()) - (taskStartDate.getHours() * 60 + taskStartDate.getMinutes());
+//       deductTimeFromTracker(freeTimeTracker, taskStartDate.getDay(), timeUsed);
+
+//       // Check if there's any remaining time on the current day
+//       if (!isTimeAvailable(freeTimeTracker, taskEndDate.getDay(), task.estimatedtime)) {
+//           currentDate.setDate(currentDate.getDate() + 1);
+//       } else {
+//           currentDate = new Date(taskEndDate);
+//       }
+
+//       task.start = taskStartDate;
+//       task.end = taskEndDate;
+
+//       // Update the task in the database
+//       const { error } = await supabase
+//           .from('tasks')
+//           .update({
+//               startdate: task.start,
+//               enddate: task.end
+//           })
+//           .eq('taskid', task.taskid);
+
+//       if (error) {
+//           console.error("Error updating task:", error.message);
+//       }
+//   }
+//   window.location.reload();
+// }
+
+function calculateTaskDuration(taskDuration, startDate, dueDate, numdays) {
+  let remainingDuration = taskDuration;
+  let currentDate = new Date(startDate);
+  let endDate = new Date(startDate);
+
+  let daysScheduled = 0;
+
+  while (daysScheduled < numdays) {
+      const dayOfWeek = currentDate.getDay();
+      const availableTime = freeTimeByDay[dayOfWeek] || 0;
+
+      if (dueDate && currentDate > dueDate) {
+          console.error("Task cannot be scheduled within the due date!");
+          return null;
+      }
+
+      if (remainingDuration > 0) {
+          if (availableTime >= remainingDuration) {
+              endDate.setMinutes(endDate.getMinutes() + remainingDuration);
+              remainingDuration = 0;
+          } else {
+              endDate.setMinutes(endDate.getMinutes() + availableTime);
+              remainingDuration -= availableTime;
+          }
+      }
+
+      // If there's no remaining duration, break out of the loop
+      if (remainingDuration <= 0) {
+          break;
+      }
+
+      currentDate.setDate(currentDate.getDate() + 1);
+      endDate = new Date(currentDate);
+      daysScheduled++;
+  }
+
+  return {
+      start: startDate,
+      end: endDate,
+  };
 }
 
-  async function updateTasksWithDates() {
-    // Sort tasks based on priority
-    const sortedTasks = [...tasks].sort((a, b) => b.priorityof - a.priorityof);
+async function updateTasksWithDates() {
+  // Sort tasks based on priority
+  const sortedTasks = [...tasks].sort((a, b) => b.priorityof - a.priorityof);
 
-    let currentDate = new Date(); // Start scheduling from today
+  let currentDate = new Date(); // Start scheduling from today
 
-    // For each task, calculate its start and end dates
-    for (const task of sortedTasks) {
-      const { start, end } = calculateTaskDuration(task.estimatedtime, currentDate, task.duedate);
+  for (let i = 0; i < sortedTasks.length; i++) {
+      const task = sortedTasks[i];
+      const result = calculateTaskDuration(task.estimatedtime, currentDate, task.duedate, task.numdays);
       
-      if (!start || !end) {
-        console.error("Couldn't schedule task:", task);
-        continue; // Skip this task if it couldn't be scheduled
+      if (!result) {
+          console.error("Couldn't schedule task:", task);
+          continue;
       }
+
+      const { start, end } = result;
 
       task.start = start;
       task.end = end;
 
-      currentDate = new Date(end); // Update the currentDate to the end date of the last scheduled task
-      currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
+      // Update the currentDate to the end date of the last scheduled task
+      currentDate = new Date(end);
+
+      // Check if there's any remaining time on the current day
+      const dayOfWeek = currentDate.getDay();
+      const availableTime = freeTimeByDay[dayOfWeek] || 0;
+      const timeUsed = (end.getHours() * 60 + end.getMinutes()) - (start.getHours() * 60 + start.getMinutes());
+      const remainingTime = availableTime - timeUsed;
+      
+      // If the next task can fit into the remaining time, schedule it on the same day
+      if (i + 1 < sortedTasks.length && sortedTasks[i + 1].estimatedtime <= remainingTime) {
+          i++; // Move to the next task
+          const nextTask = sortedTasks[i];
+          nextTask.start = currentDate;
+          nextTask.end = new Date(currentDate.getTime() + nextTask.estimatedtime * 60 * 1000);
+      } else {
+          currentDate.setDate(currentDate.getDate() + 1); // Move to the next day
+      }
 
       // Update the task in the database
       const { error } = await supabase
-        .from('tasks')
-        .update({
-          startdate: task.start,
-          enddate: task.end
-        })
-        .eq('taskid', task.taskid);
+          .from('tasks')
+          .update({
+              startdate: task.start,
+              enddate: task.end
+          })
+          .eq('taskid', task.taskid);
 
       if (error) {
-        console.error("Error updating task:", error.message);
+          console.error("Error updating task:", error.message);
       }
-    }
-    window.location.reload();
   }
+  window.location.reload();
+}
+
+function getTimeDetails(task) {
+  const start = new Date(task.start);
+  const end = new Date(task.end);
+  const dayOfWeek = start.getDay();
+  const availableTime = freeTimeByDay[dayOfWeek] || 0;
+  const timeUsed = (end.getHours() * 60 + end.getMinutes()) - (start.getHours() * 60 + start.getMinutes());
+  const remainingTime = availableTime - timeUsed;
+
+  return {
+      timeUsed,
+      remainingTime
+  };
+}
+
+  function getColorForTaskType(taskType: number) {
+    switch (taskType) {
+      case 1: return 'red';
+      case 2: return 'orange';
+      case 3: return 'darkyellow';
+      case 4: return 'green';
+      case 5: return 'blue';
+      case 6: return 'indigo';
+      case 7: return 'purple';
+      case 8: return 'violet';
+      case 9: return '#ADD8E6'; // lightblue
+      case 10: return '#90EE90'; // lightgreen
+      case 11: return '#D8BFD8'; // thistle (a light purple)
+      default: return 'white';  // Default color
+    }
+}
 
   //check if signed in
   useEffect(() => {
@@ -216,21 +406,28 @@ export default function Calendar() {
 
         if (error) throw error;
 
-        const formattedTasks = data.map(task => ({
-            title: task.taskname,
-            duedate: task.duedate,
-            start: task.startdate,
-            end: task.enddate, 
-            eventHeight: Math.min(30,MAX_EVENT_HEIGHT),
-            taskid: task.taskid,
-            priorityof: task.piorityof,
-            estimatedtime: task.estimatedtime,
-      }));
+        const formattedTasks = data.map(task => {
+          const adjustedEndDate = new Date(task.enddate);
+          adjustedEndDate.setDate(adjustedEndDate.getDate() + 1);
+
+          const taskColor = getColorForTaskType(task.tasktype);
+
+          return {
+              title: task.taskname,
+              duedate: task.duedate,
+              start: task.startdate,
+              end: adjustedEndDate,
+              eventHeight: 30,
+              taskid: task.taskid,
+              priorityof: task.piorityof,
+              estimatedtime: task.estimatedtime,
+              numdays: task.numdays,
+              color: taskColor,
+          };
+      });      
 
         setTasks(formattedTasks);
-        useEffect(() => {
-          updateTasksWithDates();
-        }, [tasks, freeTimeByDay]);
+        console.log(formattedTasks);
       } catch (error) {
         console.error("Error fetching tasks:", error.message);
       }
@@ -248,15 +445,16 @@ export default function Calendar() {
 
   function renderEventContent(eventInfo) {
     const { event } = eventInfo;
-    const height = event.extendedProps.eventHeight || 30;
-  
+    const height = 50;
+
+    const { timeUsed, remainingTime } = getTimeDetails(event);
+
     return (
-      <div style={{ height: `${height}px`, overflow: 'hidden' }}>
-        {event.title}
-        {event.estimatedtime}
-      </div>
+        <div style={{ height: `${height}px`, overflow: 'hidden' }}>
+            <b>{event.title}</b>
+        </div>
     );
-  }
+}
 
   const handleEventClick = (clickInfo) => {
     setSelectedTask(clickInfo.event.extendedProps.taskid);
@@ -266,10 +464,11 @@ export default function Calendar() {
   return (
     <>
     <div className="main-bg relative min-h-screen p-8 text-buddha-200">
-      <main className="flex min-h-screen flex-col items-center p-24 ">
-        <div className="grid grid-cols-1 sm:grid-cols-2 h-full w-full text-buddha-200">
-          <div className="sm:col-span-2">
+      <main className="calendar-container">
+        <div className="grid grid-cols-1 lg:grid-cols-2 h-full w-full text-buddha-200">
+          <div className="lg:col-span-2">
             <FullCalendar
+              height = "auto"
               plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
               headerToolbar={{
                 left: 'prev,next today',
@@ -299,11 +498,11 @@ export default function Calendar() {
             selectedDate={selectedDate}
           />
         </div>
-        <br />
-        <button className="bg-buddha-500 hover:bg-buddha-200 text-buddha-950 py-2 px-4 rounded-full" onClick={updateTasksWithDates}>
-            Update Tasks
-        </button>
       </main>
+      <button className="bg-buddha-500 hover:bg-buddha-200 text-buddha-950 py-2 px-4 rounded-full align-center" onClick={updateTasksWithDates}>
+         Update Tasks
+      </button>
+      <br />
       <Taskmenu />
     </div>
     </>
